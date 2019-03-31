af297bd3adb1951bf8ba21dc0e6073df881cd5ea
Harvey <darasimiajewole@gmail.com>
2019-03-31 18:16:03 +0100

    production changes

--- ven10/production-wsgi.py
+++ ven10/production-wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ven10.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ven10.production-settings')
 
 application = get_wsgi_application()
