83249df1c9d781dd08e340501c59966a10774a8f
Darasimi Ajewole <darasimiajewole@gmail.com>
2019-07-07 07:42:38 +0100

    empty commit

--- onlineProducts/static/onlineProducts/scripts/index.js
+++ onlineProducts/static/onlineProducts/scripts/index.js
@@ -19,20 +19,6 @@ function submit(event) {
     .done(afterSubmission)
     .fail((xhr,status,errorThrown)=> {failed(xhr,status,errorThrown,event)})
     /*
-    var image = document.querySelector("#pic");
-    FD.append("image", image.files[0]);
-    //url of Product Create api
-    var url = currentURL.origin;
-    var path = "/products/create";
-    url += path;
-    fetch(url,{
-        method: "post",
-        body: FD,
-        mode: "cors",
-    })
-    .then((response) => response.json())
-    .then((data) => afterSubmission(data))
-    
     message.textContent = "Adding New product...";
     productForm.form('reset');*/
 }
@@ -40,22 +26,13 @@ function submit(event) {
 //handles redirection submission 
 function afterSubmission(response) {
     console.log(response);
-    /*
-    var url = currentURL.origin;
-    var path = "/static/detail.html"
-    var query = "?id=";
-    var productID = data.id;
-    window.location.href = url + path + query + productID;
-     */
+    //work on after submission
     
 }
 
 //handles error from form submission
 function failed(xhr,status,errorThrown,event) {
-    alert( "Sorry, there was a problem!, check console to see what went wrong" );
-    console.log( "Error: " + errorThrown );
-    console.log( "Status: " + status );
-    console.dir( xhr );
+    alert( "Sorry, there was a problem!, kindly try again" );
     event.preventDefault();
 }
 
@@ -147,3 +124,60 @@ var valObject = {
   }
 
 productForm.form(valObject)
+
+/* 
+//retrieves product ID parameter from url
+
+function extractID() {
+    const urlString = window.location.href;
+    var url = new URL(urlString);
+    var id = url.searchParams. get ("id");
+    return id
+}
+//queries the Product Detail api which respond with all properties of a product
+function queryApi(id) {
+    //url of  Product Detail
+    const apiURL = currentURL.origin + `/products/${id}`;
+    axios.get(apiURL).then((data) => populate(data));
+}
+
+// populates the front end with response from querying Product Detail api
+function populate(data) {
+    
+    var obj = data.data;
+    var detail = document.querySelector("#detail");
+    
+    var heading = document.createElement("h3");
+    var pricePara = document.createElement("p");
+    var descriptionPara = document.createElement("p");
+    var categoryPara = document.createElement("p");
+    var descriptionPara = document.createElement("p");
+    var image  = document.createElement("img");
+    var color= obj.color;
+        
+    heading.textContent = obj.name;
+    pricePara.textContent = "Price:  " + "$" + obj.price ;
+    categoryPara.textContent = "Category:  "  + obj.category;
+    descriptionPara.textContent = "Description:  " + obj.description;
+    
+    var imageURL = currentURL.origin + obj.image;
+    image.setAttribute("src",imageURL);
+    
+    detail.append(heading,pricePara,categoryPara, descriptionPara,image)
+    
+    body = document.querySelector("body");
+    body.style.backgroundColor = color;
+    
+    }
+
+//script entry point
+function start() {
+    var id = extractID();
+    queryApi(id);
+    }
+
+var currentURL = new URL(window.location.href);
+
+start();
+
+*/
